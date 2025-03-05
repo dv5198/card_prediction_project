e8393dd1317e5e6faeda64b8fdc795f6b72123b9
Divya Nirankari <47979820+dv5198@users.noreply.github.com>
2025-03-05 22:17:08 +0530

    Update wsgi.py

--- card_prediction_project/card_prediction_project/wsgi.py
+++ card_prediction_project/card_prediction_project/wsgi.py
@@ -13,4 +13,11 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'card_prediction_project.settings')
 
+import tensorflow as tf
+tf.config.set_logical_device_configuration(
+    tf.config.list_physical_devices('CPU')[0],
+    [tf.config.LogicalDeviceConfiguration(memory_limit=300)]
+)
+
+
 application = get_wsgi_application()
